0ffcfb90c5a4c63eab5a558f5725296bf2c09f27
Anh Tuan <anhtuan9702@gmail.com>
2023-10-23 00:30:42 +0700

    Implement AES with CBC and CTR

--- compare.py
+++ compare.py
@@ -1,11 +1,60 @@
-import os
+import os, time
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-
+from cryptography.hazmat.primitives import padding
+from plaintext import plaintexts
 import my_aes as aes
 
+key = os.urandom(16)
+iv = os.urandom(16)
+
+cipher_cbc = Cipher(algorithms.AES(key), modes.CBC(iv))
+cipher_ctr = Cipher(algorithms.AES(key), modes.CTR(iv))
 
 
+def cryptography_cbc_time():
+    start = time.time()
+    for p in plaintexts:
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(bytes(p, 'latin-1')) + padder.finalize()
+        encryptor = cipher_cbc.encryptor()
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        decryptor = cipher_cbc.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+    return time.time() - start
 
 
+def my_aes_cbc_time():
+    start = time.time()
+    for p in plaintexts:
+        _, my_ciphertext = aes.encrypt(p, key, iv, mode='CBC')
+        my_plaintext = aes.decrypt(my_ciphertext, iv, key)
+    return time.time() - start
 
+
+print(f'Cryptography library - CBC mode: {cryptography_cbc_time()}s')
+print(f'My AES-CBC Implementation: {my_aes_cbc_time()}s')
+
+
+def cryptography_ctr_time():
+    start = time.time()
+    for p in plaintexts:
+        encryptor = cipher_ctr.encryptor()
+        ciphertext = encryptor.update(bytes(p, 'latin-1')) + encryptor.finalize()
+        decryptor = cipher_ctr.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+    return time.time() - start
+
+
+def my_aes_ctr_time():
+    start = time.time()
+    for p in plaintexts:
+        _, my_ciphertext = aes.encrypt(p, key, iv, mode='CTR')
+        my_plaintext = aes.decrypt(my_ciphertext, iv, key, mode='CTR')
+    return time.time() - start
+
+
+print(f'Cryptography library - CTR mode: {cryptography_ctr_time()}s')
+print(f'My AES-CTR Implementation: {my_aes_ctr_time()}s')
 
---
+++ plaintext.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+plaintexts = [
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+"AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+"AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+"AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+    "AES is a variant of the Rijndael block cipher developed by two Belgian cryptographers, "
+    "Joan Daemen and Vincent Rijmen, who submitted a proposal to NIST during the AES selection process.",
+    "Rijndael is a family of ciphers with different key and block sizes.",
+    "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, "
+    "but three different key lengths: 128, 192 and 256 bits.",
+    "n cryptography, a block cipher mode of operation is an algorithm that uses a block cipher to provide "
+    "information security such as confidentiality or authenticity",
+
+]
